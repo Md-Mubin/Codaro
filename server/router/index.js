987731ee333c5e04f62724b596d0febf00bb074c
Md Mubin <fontdevmubin@gmail.com>
2025-06-04 21:07:41 +0600

    modified:   server/router/index.js

--- server/router/index.js
+++ server/router/index.js
@@ -4,4 +4,12 @@ const router = express.Router()
 
 router.use(process.env.VERSION_API, apiRoute)
 
+router.use("/", (req,res)=>{
+    res.send("Hello! Welcome To The Server 🙂")
+})
+
+router.use("*", (req,res)=>{
+    res.status(404).send("Page Not Found")
+})
+
 module.exports = router
